2b76ccda66e75d666c6cb03254844a7cfa14a64c
nandini965 <nandhini965@gmail.com>
2023-08-17 23:52:54 +0530

    filebeat

--- tags.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  tags = {
-      business_unit = "ecommerce"
-      business_type = "retail"
-      project_name  = "RoboShop"
-      cost_center   = 100
-      env           = var.env
-     }
- }
