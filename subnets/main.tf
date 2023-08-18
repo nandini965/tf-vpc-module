c3c2905a7fb96b39e88b5a89c1fa11e8fa4dc755
nandini965 <nandhini965@gmail.com>
2023-08-18 19:34:03 +0530

    filebeat

--- subnets/main.tf
+++ subnets/main.tf
@@ -3,7 +3,6 @@
    vpc_id     = "var.vpc_id"
    cidr_block = var.cidr_block[count.index]
 
-   tags = {
-     Name = merge(var.tags, { Name = "${var.env}-${var.name}-subnet-${count.index}" })
-   }
+   tags = merge(var.tags, { Name = "${var.env}-${var.name}-subnet-${count.index}" })
+
  }
