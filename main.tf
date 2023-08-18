98bf091de36c6bddb29a30a6514bd306e6c84d97
nandini965 <nandhini965@gmail.com>
2023-08-18 19:43:30 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main" {
     for_each   = var.subnets
     vpc_id     = "aws_vpc.main.id"
     cidr_block = each.value["cidr_block"]
-    name       = each.value["name"]
+    name       = each.value.name
     tags       = var.tags
     env        = var.env
   }
