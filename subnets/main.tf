ad0e976b3fb46255d41805017ef07cc631393dd6
nandini965 <nandhini965@gmail.com>
2023-08-18 19:19:33 +0530

    filebeat

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,8 +1,9 @@
  resource "aws_subnet" "main" {
-   count = length(var.cidr_block)
-  vpc_id     = "var.vpc_id"
-  cidr_block = var.cidr_block[count.index]
+   count      = length(var.cidr_block)
+   vpc_id     = "var.vpc_id"
+   cidr_block = var.cidr_block[count.index]
 
-  tags = {
-    Name = merge(var.tags, { Name = "${var.env}-${var.name}-subnet-${count.index}" })
-  }
+   tags = {
+     Name = merge(var.tags, { Name = "${var.env}-${var.name}-subnet-${count.index}" })
+   }
+ }
