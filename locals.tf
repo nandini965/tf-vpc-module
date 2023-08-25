163a79bb05b74aa841796f54980bf0e1f40a2283
nandini965 <nandhini965@gmail.com>
2023-08-25 14:14:59 +0530

    filebeat

---
+++ locals.tf
@@ -0,0 +1,1 @@
+all_private_subnets_ids = concat(module.subnets["db"].route_table_ids,module.subnets["app"].route_table_ids,module.subnets["web"].route_table_ids)
--- main.tf
+++ main.tf
@@ -41,3 +41,19 @@ resource "aws_nat_gateway" "ngw" {
   # on the Internet Gateway for the VPC.
 
 }
+
+
+  resource "aws_route" "igw" {
+  count = length(module.subnets["public"].route_table_ids)
+  route_table_id =  module.subnets["public"].route_table_ids[count.index]
+   gateway_id = aws_internet_gateway.igw.id
+    destination_cidr_block = "0.0.0.0/0"
+    }
+
+resource "aws_route" "ngw" {
+  count = length(local.all_private_subnet_ids)
+  route_table_id =  local.all_private_subnet_ids
+  gateway_id = element(aws_nat_gateway.ngw.id,count_index)
+  destination_cidr_block = "0.0.0.0/0"
+}
+
