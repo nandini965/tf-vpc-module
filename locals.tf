15081f7a66f2771f70315bc836d4649e5872021b
nandini965 <nandhini965@gmail.com>
2023-08-25 14:41:28 +0530

    filebeat

--- locals.tf
+++ locals.tf
@@ -1,1 +1,3 @@
- all_private_subnet_ids = concat(module.subnets["db"].route_table_ids,module.subnets["app"].route_table_ids,module.subnets["web"].route_table_ids)
+locals {
+  all_private_subnet_ids = concat(module.subnets["db"].route_table_ids, module.subnets["app"].route_table_ids, module.subnets["web"].route_table_ids)
+}
