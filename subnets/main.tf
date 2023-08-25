2ee16ec193cdbf61b71c44905620f85ac3c63000
nandini965 <nandhini965@gmail.com>
2023-08-25 13:22:54 +0530

    filebeat

--- subnets/main.tf
+++ subnets/main.tf
@@ -7,6 +7,13 @@
    tags = merge(var.tags, { Name = "${var.env}-${var.name}-subnet-${count.index+1}" })
 
  }
+
+
+ resource "aws_route_table_association" "associate" {
+   count          = length(var.cidr_block)
+   subnet_id      = aws_subnet.main[count.index].id
+   route_table_id = aws_route_table.main[count.index].id
+ }
  resource "aws_route_table" "main" {
    count      = length(var.cidr_block)
     vpc_id = var.vpc_id
